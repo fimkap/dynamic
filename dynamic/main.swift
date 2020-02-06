317d277305838889f54976eba89d50b3c223f00e
Efim <efim@igroup-il.com>
2020-02-06 23:13:18 +0200

    Initial Commit

---
+++ dynamic/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  dynamic
+//
+//  Created by Efim Polevoi on 06/02/2020.
+//  Copyright © 2020 Efim Polevoi. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
